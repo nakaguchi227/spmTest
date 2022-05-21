e7deaa3d1199f223bc4727a00ab1eebf12cf0340
shota.nakaguchi <nakaguchi.shot227@gmail.com>
2022-05-22 01:21:52 +0900

    first

---
+++ spm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spm
+//
+//  Created by 中口翔太 on 2022/05/21.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
